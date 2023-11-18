8057e6ee376294ab27d27f0de0b2aec2ddcf4ce2
yarma <yarmatua@gmail.com>
2023-11-18 13:09:54 +0200

    added test for method getMessageById

--- tests/TelegramCSTest.php
+++ tests/TelegramCSTest.php
@@ -33,4 +33,25 @@ class TelegramCSTest extends TestCase
 
         $this->assertEquals(40, $service->getMessages()->count());
     }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function test_get_message_by_id(): void
+    {
+        $service = new TelegramCS('spotifynewss');
+
+        $message = $service->getMessageById(308);
+
+        $this->assertEquals(308, $message->id);
+        $this->assertEquals('¶Spotify Team™ pinned a photo', $message->lineText);
+        $this->assertTrue($message->service);
+
+        $secondMessage = $service->getMessageById(307);
+        $this->assertEquals(307, $secondMessage->id);
+        $this->assertFalse($secondMessage->service);
+        $this->assertEquals('▄︻デE̷t̷h̷i̷c̷a̷l̷ ̷ H̷a̷c̷k̷e̷r̷══━一 ⁪⁬⁮⁮', $secondMessage->author);
+        $this->assertCount(1, $secondMessage->photos);
+        $this->assertFalse($secondMessage->edited);
+    }
 }
